7598a07d5234f98e85d6c800c8c36cd5b0d63f66
Debasmita C <debasmitac2004@gmail.com>
2025-08-05 18:45:55 +0530

    Updated main.dart
    removed unused import

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'cubit/canvas_cubit.dart';
 import 'ui/screens/splash_screen.dart';
-import 'utils/custom_snackbar.dart';
 
 void main() {
   runApp(const MyApp());
